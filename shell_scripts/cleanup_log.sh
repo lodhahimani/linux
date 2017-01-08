6af949efe64cb67d4c86abec8da7fca2756c829c
root <root@ip-172-31-37-62.us-west-2.compute.internal>
2017-01-08 00:30:36 +0000

    test_arg is script to pass more than 10 positional parameters main_ is calling test_arg shell script

--- shell_scripts/cleanup_log.sh
+++ shell_scripts/cleanup_log.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cat /dev/null > /var/log/messages
-echo "/var/log/messages are cleaned"
+DIR_PATH=$1
+if [ -z "$DIR_PATH" ]; then
+	echo "usage: cleanup_log <pass the pah>"
+else
+
+echo $DIR_PATH
+cat /dev/null > $DIR_PATH
+echo "Given $DIR_PATH is cleaned"
+fi
